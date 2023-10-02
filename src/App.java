5ee040b7f310f4636e3754fe1a7ef552f130f177
Leonardo <116222412+leopsdev@users.noreply.github.com>
2023-10-02 12:32:50 -0300

    conexao com o database
    usuario DAO com insert, update, select, select all e delete.

--- src/App.java
+++ src/App.java
@@ -1,17 +1,25 @@
+import java.sql.Connection;
+
+import dao.Conexao;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
         Vendedor vend1 = new Vendedor("Caixa.com","caixas.com","caixacom@gmail.com",01,"Caixas.com","Loja de caixas");
-        
+
         Produto prod1 = vend1.cadastrarProduto();
 
-        Cliente cliente1 = new Cliente("Leonardo","Triangulo das Bermudas","leopereiraesilca@gmail.com",40020322,"null");
+        Cliente cliente1 = new Cliente("teste1","Triangulo das Bermudas","leopereiraesilca@gmail.com",40020322,"null");
+
+        Connection conexao = new Conexao().getConnection();
+        UsuarioDAO usuarioDao = new UsuarioDAO(conexao);
+        usuarioDao.insertCliente(cliente1);
+        
 
         cliente1.adicionarListaDeDesejos(prod1);
 
         cliente1.adicionarAoCarrinho(prod1, vend1);
         
-        Pedido ped1 = cliente1.fazerPedido();
-          
+        conexao.close();
     }
 }
---
+++ src/dao/Conexao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public Connection getConnection()throws SQLException{
+        Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5433/frald","postgres","leo123");
+        return conexao;
+    }
+}
+
