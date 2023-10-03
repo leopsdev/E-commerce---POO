1e1cfe828f7d433d61cfc423de76c7cfb44b4e03
Leonardo <116222412+leopsdev@users.noreply.github.com>
2023-10-03 09:57:01 -0300

    fix idbug e vendedor inserts
    vendedor:
    insert
    update
    delete
    select
    select all

--- src/App.java
+++ src/App.java
@@ -4,24 +4,36 @@ import dao.Conexao;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        Vendedor vend1 = new Vendedor("nome","nome.com","nome@gmail.com","4002",4002,"nome, um nome pra ti");
 
-        Produto prod1 = vend1.cadastrarProduto();
+        Vendedor vend1 = new Vendedor("nome2","nome.com","nome@gmail.com","4002",4002,"nome, um nome pra ti");
 
-        Cliente cliente1 = new Cliente("teste1","rua teste, avenida teste","teste@gmail.com","123teste",40028922);
+        //Produto prod1 = vend1.cadastrarProduto();
+
+        Cliente cliente1 = new Cliente("teste6","rua teste, avenida teste","teste@gmail.com","123teste",40028922);
 
         Connection conexao = new Conexao().getConnection();
         UsuarioDAO usuarioDao = new UsuarioDAO(conexao);
-        usuarioDao.insertCliente(cliente1);
+
+        // usuarioDao.insertCliente(cliente1);
+        // usuarioDao.insertVendedor(vend1);
         
+        // System.out.println("Contas Clientes:");
+        // for (Cliente atual : usuarioDao.selectAllCliente()) {
+        //     System.out.println("Email:"+atual.getEmail()+" Nome:"+atual.getNome()+ " id:" + atual.getId());
+        // }
 
-        cliente1.adicionarListaDeDesejos(prod1);
+        // System.out.println("Contas Vende:");
+        // for (Vendedor atual : usuarioDao.selectAllVendedor()) {
+        //     System.out.println("Email:"+atual.getEmail()+" Nome:"+atual.getNome()+ " id:" + atual.getId());
+        // }
 
-        boolean verificacao = cliente1.getCarrinhoDeCompras().adicionarAoCarrinho(prod1);
-        if(verificacao){
-            cliente1.realizarCompra();
-        }
+
+        //cliente1.adicionarListaDeDesejos(prod1);
+
+        //boolean verificacao = cliente1.getCarrinhoDeCompras().adicionarAoCarrinho(prod1);
+        // if(verificacao){
+        //     cliente1.realizarCompra();
+        // }
         
         conexao.close();
     }
--- src/UsuarioDAO.java
+++ src/UsuarioDAO.java
@@ -21,7 +21,8 @@ public class UsuarioDAO {
         statement.setLong(4, cliente.getCpf());
         statement.setString(5, cliente.getEndereco());
         statement.execute();
-        
+
+        cliente.setId(idTabelaCliente(cliente));
     }
 
     /* preciso reformular */
@@ -36,14 +37,16 @@ public class UsuarioDAO {
         statement.setString(5, vendedor.getDescricao_empresa());
         statement.setString(6, vendedor.getEndereco());
         statement.execute();
+
+        vendedor.setId(idTabelaVendedor(vendedor));
         
     }
 
-    public boolean existeUsuario(Usuario usuario) throws SQLException{
+    public boolean existeCliente(Cliente cliente) throws SQLException{
         String sql = "select * from usuario where email = ? and senha = ?;";
         PreparedStatement statement = conection.prepareStatement(sql);
-        statement.setString(1, usuario.getEmail());
-        statement.setString(2, usuario.getSenha());
+        statement.setString(1, cliente.getEmail());
+        statement.setString(2, cliente.getSenha());
         statement.execute();
         
         ResultSet result = statement.getResultSet();
@@ -51,22 +54,67 @@ public class UsuarioDAO {
         return result.next();
     }
 
-    /* metodo não finalizado ainda */
-    public void updateUsuario(Usuario usuario) throws SQLException{
+    public int idTabelaCliente(Cliente cliente) throws SQLException{
+        String sql = "select * from usuario where email = ?;";
+        PreparedStatement statement = conection.prepareStatement(sql);
+        statement.setString(1, cliente.getEmail());
+        ArrayList<Cliente> foo = pesquisaCliente(statement);
+        
+        return foo.get(0).getId();
+    }
+
+    public int idTabelaVendedor(Vendedor vendedor) throws SQLException{
+        String sql = "select * from vendedor where email = ?;";
+        PreparedStatement statement = conection.prepareStatement(sql);
+        statement.setString(1, vendedor.getEmail());
+        ArrayList<Vendedor> foo = pesquisaVendedor(statement);
+        
+        return foo.get(0).getId();
+    }
+
+    public boolean existeVendedor(Vendedor vendedor) throws SQLException{
+        String sql = "select * from vendedor where email = ? and senha = ?;";
+        PreparedStatement statement = conection.prepareStatement(sql);
+        statement.setString(1, vendedor.getEmail());
+        statement.setString(2, vendedor.getSenha());
+        statement.execute();
+        
+        ResultSet result = statement.getResultSet();
+
+        return result.next();
+    }
+
+    public void updateCliente(Cliente cliente) throws SQLException{
         String sql = "update usuario set email = ? and senha = ? where id = ?;";
         PreparedStatement statement = conection.prepareStatement(sql);
-        statement.setString(1, usuario.getEmail());
-        statement.setString(2, usuario.getSenha());
-        //statement.setLong(3, usuario.getProximoID); -> não tem getter para pegar o id do usuario, 
-        //tbm não há garantia que esses ids(do codigo e da tabela) sejam iguais. observar isso tbm;
+        statement.setString(1, cliente.getEmail());
+        statement.setString(2, cliente.getSenha());
+        statement.setLong(3, cliente.getId());
         statement.execute();
     }
 
-    public void deliteUsuario(Usuario usuario) throws SQLException{
+    public void updateVendedor(Vendedor vendedor) throws SQLException{
+        String sql = "update usuario set email = ? and senha = ? where id = ?;";
+        PreparedStatement statement = conection.prepareStatement(sql);
+        statement.setString(1, vendedor.getEmail());
+        statement.setString(2, vendedor.getSenha());
+        statement.setLong(3, vendedor.getId());
+        statement.execute();
+    }
+
+    public void deleteCliente(Cliente cliente) throws SQLException{
         String sql = "delete from usuario where id = ?;";
         
         PreparedStatement statement = conection.prepareStatement(sql);
-        //statement.setInt(1, usuario.getProximoID());
+        statement.setInt(1, cliente.getId());
+        statement.execute();
+    }
+
+    public void deleteVendedor(Vendedor vendedor) throws SQLException{
+        String sql = "delete from usuario where id = ?;";
+        
+        PreparedStatement statement = conection.prepareStatement(sql);
+        statement.setInt(1, vendedor.getId());
         statement.execute();
     }
 
@@ -75,13 +123,17 @@ public class UsuarioDAO {
         
         PreparedStatement statement = conection.prepareStatement(sql);
 
+        return pesquisaCliente(statement);
+    }
+
+    private ArrayList<Cliente> pesquisaCliente(PreparedStatement statement) throws SQLException {
         ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 
         statement.execute();
         ResultSet results = statement.getResultSet();
 
         while (results.next()) {
-            //int id = results.getInt("id");
+            int id = results.getInt("id");
             String nome = results.getString("nome");
             String email = results.getString("email");
             String senha = results.getString("senha");
@@ -89,10 +141,42 @@ public class UsuarioDAO {
             String endereco = results.getString("endereco");
 
             Cliente clienteDoBancoDB = new Cliente(nome,endereco,email,senha,cpf);
+            clienteDoBancoDB.setId(id);
             clientes.add(clienteDoBancoDB);
         }
 
         return clientes;
+    }
+
+    public ArrayList<Vendedor> selectAllVendedor() throws SQLException{
+        String sql = "select * from vendedor;";
+        
+        PreparedStatement statement = conection.prepareStatement(sql);
+
+        return pesquisaVendedor(statement);
+    }
+
+    private ArrayList<Vendedor> pesquisaVendedor(PreparedStatement statement) throws SQLException {
+        ArrayList<Vendedor> vendedors = new ArrayList<Vendedor>();
+
+        statement.execute();
+        ResultSet results = statement.getResultSet();
+
+        while (results.next()) {
+            int id = results.getInt("id");
+            String nome = results.getString("nome_loja");
+            String email = results.getString("email");
+            String senha = results.getString("senha");
+            int cnpj = results.getInt("cnpj");
+            String endereco = results.getString("endereco");
+            String descricao_empresa = results.getString("descricao_empresa");
+
+            Vendedor vendedorDoBancoDB = new Vendedor(nome,endereco,email,senha,cnpj,descricao_empresa);
+            vendedorDoBancoDB.setId(id);
+            vendedors.add(vendedorDoBancoDB);
+        }
+
+        return vendedors;
     }
 
 
